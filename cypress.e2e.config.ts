7f44bac8afe2768f16ae92726116dfb239e1a8ae
Paul D'Ambra <paul@posthog.com>
2022-06-24 23:01:07 +0200

    is this causing slowdown?

--- cypress.e2e.config.ts
+++ cypress.e2e.config.ts
@@ -91,20 +91,6 @@ export default defineConfig({
                 },
             })
 
-            on('after:spec', (_, results) => {
-                if (results && results.video) {
-                    // Do we have failures for any retry attempts?
-                    const failures = results.tests.some((test) =>
-                        test.attempts.some((attempt) => attempt.state === 'failed')
-                    )
-
-                    if (!failures) {
-                        // delete the video if the spec passed and no tests retried
-                        return fs.unlinkSync(results.video)
-                    }
-                }
-            })
-
             return config
         },
         baseUrl: 'http://localhost:8000',
